a72480a1503d4d00be41e3adfb085836612686af
elvito <elvito@users.noreply.github.com>
2025-04-25 20:17:30 +0200

    .

--- install/librespeed-install.sh
+++ install/librespeed-install.sh
@@ -14,28 +14,34 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-#not neccessary
+$STD apt-get install -y \
+  caddy \
+  php-fpm
 msg_ok "Installed Dependencies"
 
 msg_info "Installing librespeed"
 temp_file=$(mktemp)
 RELEASE=$(curl -fsSL https://api.github.com/repos/librespeed/speedtest-go/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4)}')
 curl -fsSL "https://github.com/librespeed/speedtest-go/releases/download/v${RELEASE}/speedtest-go_${RELEASE}_linux_386.tar.gz" -o $temp_file
-mkdir -p /opt/librespeed
+mkdir -p /opt/librespeed/assets
 tar -xvzf $temp_file -C /opt/librespeed
 chmod +x /opt/librespeed/speedtest-backend
 echo "${RELEASE}" >"/opt/librespeed/librespeed_version.txt"
+go mod init librespeed
+go get github.com/boltdb/bolt
+chown -R 1000:1000 /opt/librespeed
+chmod -R 755 /opt/librespeed
 msg_ok "Installation completed"
 
 msg_info "Creating systemd Service"
-cat <<EOF >/etc/systemd/system/librespeed.service
+cat <<EOF >/etc/systemd/system/librespeed-backend.service
 [Unit]
-Description=librespeed Service
+Description=Librespeed Backend Service
 After=network.target
 
 [Service]
 WorkingDirectory=/opt/librespeed
-### ExecStart=/opt/librespeed/
+ExecStart=/opt/librespeed/
 Restart=always
 User=root
 Group=root
