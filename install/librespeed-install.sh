aa15219c28382665c5883486983dad5fb1f2f40b
elvito <elvito@users.noreply.github.com>
2025-04-25 13:53:07 +0200

    librespeed install.sh

---
+++ install/librespeed-install.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2025 community-scripts ORG
+# Author: elvito
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/librespeed/speedtest-go
+
+source /dev/stdin <<< "$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_info "Installing Dependencies"
+$STD curl -fsSL https://packages.microsoft.com/config/debian/12/packages-microsoft-prod.deb -o packages-microsoft-prod.deb
+$STD dpkg -i packages-microsoft-prod.deb
+$STD apt-get update
+$STD apt-get install -y \
+  dotnet-sdk-8.0 \
+  aspnetcore-runtime-8.0
+  msg_ok "Installed Dependencies"
+
+msg_info "Installing Umlautadaptarr"
+temp_file=$(mktemp)
+RELEASE=$(curl -fsSL https://api.github.com/repos/librespeed/speedtest-go/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4)}')
+curl -fsSL "https://github.com/librespeed/speedtest-go/releases/download/v${RELEASE}/speedtest-go_${RELEASE}_darwin_amd64.tar.gz" -o $temp_file
+###unzip -qj $temp_file '*/**' -d /opt/UmlautAdaptarr
+echo "${RELEASE}" >"/opt/UmlautAdaptarr_version.txt"
+msg_ok "Installation completed"
+
+msg_info "Creating appsettings.json"
+cat <<EOF >/opt/librespeed/appsettings.json
+
+EOF
+msg_ok "appsettings.json created"
+
+msg_info "Creating systemd Service"
+cat <<EOF >/etc/systemd/system/umlautadaptarrlibrespeed.service
+[Unit]
+Description=UmlautAdaptarr Service
+After=network.target
+
+[Service]
+WorkingDirectory=/opt/librespeed
+### ExecStart=/usr/bin/dotnet /opt/librespeed/
+Restart=always
+User=root
+Group=root
+### Environment=
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl -q --now enable librespeed
+msg_ok "Created systemd Service"
+
+motd_ssh
+customize
+
+msg_info "Cleaning up"
+rm -f $temp_file
+$STD apt-get -y autoremove
+$STD apt-get -y autoclean
+msg_ok "Cleaned"
