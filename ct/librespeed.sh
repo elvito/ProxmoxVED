bced7c199a0033d459f6d4a8562d7b112d3bd073
elvito <elvito@users.noreply.github.com>
2025-04-26 10:49:04 +0200

    wrong file in installation check

--- ct/librespeed.sh
+++ ct/librespeed.sh
@@ -24,7 +24,7 @@ function update_script() {
     check_container_storage
     check_container_resources
 
-    if [[ ! -f /opt/librespeed/settings.toml ]]; then
+    if [[ ! -f /opt/librespeed/index.html ]]; then
         msg_error "No ${APP} Installation Found!"
         exit
     fi
