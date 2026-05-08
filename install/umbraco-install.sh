8aaa608c08122269c6212626a470661cf3d55532
montagneid <joost@montagnes.nl>
2026-05-08 15:26:09 +0200

    Change license url

--- install/umbraco-install.sh
+++ install/umbraco-install.sh
@@ -2,7 +2,7 @@
 
 # Copyright (c) 2021-2026 community-scripts ORG
 # Author: Joost van den Berg
-# License: MIT | https://github.com/--full/ProxmoxVED/raw/main/LICENSE
+# License: MIT | https://github.com/montagneid/ProxmoxVED/raw/main/LICENSE
 # Source: https://github.com/umbraco/Umbraco-CMS
 
 source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
