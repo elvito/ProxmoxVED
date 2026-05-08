5881069a6b6c1a46aa524583501683886e430e5f
montagneid <joost@montagnes.nl>
2026-05-08 14:52:54 +0200

    .net install

--- install/umbraco-install.sh
+++ install/umbraco-install.sh
@@ -25,24 +25,12 @@ $STD apt-get install -y \
 msg_ok "Installed Dependencies"
 
 msg_info "Installing .NET SDK 10.0"
-# Get OS version info which adds the $ID and $VERSION_ID variables
-source /etc/os-release
+wget https://packages.microsoft.com/config/debian/13/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
 
-# Download the Microsoft keys
-sudo apt-get install -y gpg wget
-wget https://packages.microsoft.com/keys/microsoft.asc
-cat microsoft.asc | gpg --dearmor -o microsoft.asc.gpg
-
-# Add the Microsoft repository to the system's sources list
-wget https://packages.microsoft.com/config/$ID/$VERSION_ID/prod.list
-sudo mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
-
-# Move the key to the appropriate place
-sudo mv microsoft.asc.gpg $(cat /etc/apt/sources.list.d/microsoft-prod.list | grep -oP "(?<=signed-by=).*(?=\])")
-
-# Update packages and install .NET
 sudo apt-get update && \
-  sudo apt-get install -y {dotnet-package}
+  sudo apt-get install -y dotnet-sdk-10.0
 msg_ok "Installed .NET SDK 10.0"
 
 msg_info "Installing dotnet Umbraco templates and create project (Patience)"
