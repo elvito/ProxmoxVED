90625cb891120e0d442c2c81500a6cffa63fd1f0
rafspiny <rafspiny@gmail.com>
2026-05-28 23:22:08 +0200

    Possible fixes

--- install/grav-install.sh
+++ install/grav-install.sh
@@ -32,7 +32,7 @@ msg_ok "PHP installed"
 # Setup App
 msg_info "Setup ${APPLICATION}"
 RELEASE=$(curl -s "https://api.github.com/repos/getgrav/grav/releases/latest" | grep '"tag_name"' | awk -F'"' '{print $4}')
-curl -fsSL "https://getgrav.org/download/core/grav-admin/latest" -o /tmp/grav-admin.zip
+curl -fsSL "https://github.com/getgrav/grav/releases/download/${RELEASE}/grav-admin-v${RELEASE}.zip" -o /tmp/grav-admin.zip
 unzip -q /tmp/grav-admin.zip -d /tmp/
 mv /tmp/grav-admin /opt/${APPLICATION,,}
 chown -R www-data:www-data /opt/${APPLICATION,,}
@@ -54,69 +54,47 @@ server {
     index index.html index.htm index.php;
 
     location / {
-        # enable for basic auth
-        #auth_basic "Restricted";
-        #auth_basic_user_file /config/nginx/.htpasswd;
-
-        try_files $uri $uri/ /index.html /index.htm /index.php$is_args$args;
+        try_files \$uri \$uri/ /index.html /index.htm /index.php\$is_args\$args;
     }
 
-    ## https://github.com/getgrav/grav/blob/develop/webserver-configs/nginx.conf
-
     ## Begin - Security
-    # deny all direct access for these folders
     location ~* /(\.git|cache|bin|logs|backup|tests)/.*$ { return 403; }
-    # deny running scripts inside core system folders
     location ~* /(system|vendor)/.*\.(txt|xml|md|html|json|yaml|yml|php|pl|py|cgi|twig|sh|bat)$ { return 403; }
-    # deny running scripts inside user folder
     location ~* /user/.*\.(txt|md|json|yaml|yml|php|pl|py|cgi|twig|sh|bat)$ { return 403; }
-    # deny access to specific files in the root folder
     location ~ /(LICENSE\.txt|composer\.lock|composer\.json|nginx\.conf|web\.config|htaccess\.txt|\.htaccess) { return 403; }
     ## End - Security
 
-    ## https://learn.getgrav.org/17/webservers-hosting/servers/nginx#nginx-cache-headers-for-assets
     ## Begin - Caching
     location ~* ^/forms-basic-captcha-image.jpg$ {
-        try_files $uri $uri/ /index.php$is_args$args;
+        try_files \$uri \$uri/ /index.php\$is_args\$args;
     }
 
     location ~* \.(?:ico|css|js|gif|jpe?g|png)$ {
-            expires 30d;
-            add_header Vary Accept-Encoding;
-            log_not_found off;
+        expires 30d;
+        add_header Vary Accept-Encoding;
+        log_not_found off;
     }
 
     location ~* ^.+\.(?:css|cur|js|jpe?g|gif|htc|ico|png|html|xml|otf|ttf|eot|woff|woff2|svg)$ {
-            access_log off;
-            expires 30d;
-            add_header Cache-Control public;
-
-
-    ## No need to bleed constant updates. Send the all shebang in one
-    ## fell swoop.
-            tcp_nodelay off;
-
-    ## Set the OS file cache.
-            open_file_cache max=3000 inactive=120s;
-            open_file_cache_valid 45s;
-            open_file_cache_min_uses 2;
-            open_file_cache_errors off;
+        access_log off;
+        expires 30d;
+        add_header Cache-Control public;
+        tcp_nodelay off;
+        open_file_cache max=3000 inactive=120s;
+        open_file_cache_valid 45s;
+        open_file_cache_min_uses 2;
+        open_file_cache_errors off;
     }
     ## End - Caching
 
     location ~ ^(.+\.php)(.*)$ {
-        # enable the next two lines for http auth
-        #auth_basic "Restricted";
-        #auth_basic_user_file /config/nginx/.htpasswd;
-
         fastcgi_split_path_info ^(.+\.php)(.*)$;
-        if (!-f $document_root$fastcgi_script_name) { return 404; }
-        fastcgi_pass 127.0.0.1:9000;
+        if (!-f \$document_root\$fastcgi_script_name) { return 404; }
+        fastcgi_pass unix:/run/php/php${PHP_VER}-fpm.sock;
         fastcgi_index index.php;
         include /etc/nginx/fastcgi_params;
     }
 
-    # deny access to .htaccess/.htpasswd files
     location ~ /\.ht {
         deny all;
     }
