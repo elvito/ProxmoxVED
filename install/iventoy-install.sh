376ba866c4970fda722bb7f1470e0ff5acf3b9e8
MickLesk <mickey.leskowitz@gmail.com>
2026-06-29 16:44:34 +0200

    iventoy ref

--- install/iventoy-install.sh
+++ install/iventoy-install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# Copyright (c) 2021-2026 tteck
+# Copyright (c) 2021-2026 community-scripts ORG
 # Author: tteck (tteckster)
-# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
 # Source: https://www.iventoy.com/en/index.html
 
 source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
@@ -13,14 +13,8 @@ setting_up_container
 network_check
 update_os
 
-RELEASE=$(curl -fsSL https://api.github.com/repos/ventoy/pxe/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
-msg_info "Installing iVentoy v${RELEASE}"
+fetch_and_deploy_gh_release "iventoy" "ventoy/PXE" "prebuild" "latest" "/opt/iventoy" "iventoy-*-linux-free.tar.gz"
 mkdir -p /opt/iventoy/{data,iso}
-curl -fsSL "https://github.com/ventoy/PXE/releases/download/v${RELEASE}/iventoy-${RELEASE}-linux-free.tar.gz" -o "iventoy-${RELEASE}-linux-free.tar.gz"
-tar -C /tmp -xzf iventoy*.tar.gz
-mv /tmp/iventoy*/* /opt/iventoy/
-rm -rf iventoy*.tar.gz
-msg_ok "Installed iVentoy"
 
 msg_info "Creating Service"
 cat <<EOF >/etc/systemd/system/iventoy.service
@@ -28,15 +22,20 @@ cat <<EOF >/etc/systemd/system/iventoy.service
 Description=iVentoy PXE Booter
 Documentation=https://www.iventoy.com
 Wants=network-online.target
+After=network-online.target
+
 [Service]
 Type=forking
+WorkingDirectory=/opt/iventoy
 Environment=IVENTOY_API_ALL=1
 Environment=IVENTOY_AUTO_RUN=1
 Environment=LIBRARY_PATH=/opt/iventoy/lib/lin64
 Environment=LD_LIBRARY_PATH=/opt/iventoy/lib/lin64
-ExecStart=sh ./iventoy.sh -R start
-WorkingDirectory=/opt/iventoy
+ExecStart=/bin/sh /opt/iventoy/iventoy.sh -R start
+ExecStop=/bin/sh /opt/iventoy/iventoy.sh stop
 Restart=on-failure
+RestartSec=5
+
 [Install]
 WantedBy=multi-user.target
 EOF
